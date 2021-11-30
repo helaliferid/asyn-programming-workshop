f8174dd639485935f6ad426d50e5af8b8a2cb23b
= <=>
2021-11-30 17:13:03 +0100

    server scaffolding

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const http=require('http');
+const url = require('url');
+
+const host='http://localhost';
+const port="8090";
+
+
+const server=http.createServer(function(req,res){
+    let path=url.parse(req.url,true);
+    let method = req.method;
+
+    res.setHeader('Content-Type','application/json');
+
+    res.end(`{
+        "path":"${JSON.stringify(path)}",
+        "method":"${method}"
+    }`
+        )
+});
+
+server.listen(port,function(){
+    console.log(`Server listening on ${host}:${port}`)
+});
+
+//TODO: completer l'implementation de ce
+// server afin de repondre sur GET /todos
+// et POS /todo ({task:'something'})
+// et avec fetch ajouter et recuperer les todos 
+// d'une maniere asynchrone (DOM)
+
+// fetch('http://example.com/songs')
+// 	.then(response => response.json())
+// 	.then(data => console.log(data))
+// 	.catch(err => console.error(err));
